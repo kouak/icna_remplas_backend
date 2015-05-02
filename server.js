e66f69d5d19aabc40412f600266ea256a1b64dce
Benjamin Beret <benjamin.beret@gmail.com>
2015-05-02 15:40:33 +0200

    File structure: routes, frontend

--- server.js
+++ server.js
@@ -13,13 +13,12 @@ var LocalStrategy = require('passport-local').Strategy;
 var dbConfig = require('./app/config').dbConfig;
 var models = require('./app/models');
 
+// Load routes
+var routes = require('./app/routes');
+
 models.init();
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'app/views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -27,10 +26,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+// Load static app
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
