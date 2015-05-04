b3fd1656837c4ebbcaa107ccaa95eac6fef1f3a7
Benjamin Beret <benjamin.beret@gmail.com>
2015-05-04 23:02:43 +0200

    Refactor tests

--- test/models/user.js
+++ test/models/user.js
@@ -7,15 +7,12 @@ var knex = initDb.knex;
 var User = require(appSettings.ROOTDIR + '/app/models/user');
 
 describe('User model', function() {
-
-
   it('should load', function(done) {
     should.exist(User);
     done();
   });
 
   describe('loaded', function() {
-
     describe('saving', function() {
 
       beforeEach(function(done) {
@@ -47,39 +44,36 @@ describe('User model', function() {
         return User.forge(u).save().should.be.rejectedWith(/ValidationError/);
       });
 
-      it('should encrypt password', function() {
-        var u = {
-          name: 'bla',
-          firstName: 'blabla',
-          email: 'a@a.com',
-          password: '123456',
-          teamId: 2
-        };
-
-        return User.forge(u).save() // Save our user
-        .then(function(user) {
-          return User.findOne({id: user.id}).then(function(ret) {
-            return ret.get('password').should.not.be.equal('123456');
-          });
-        });
-
+    });
+    
+    describe('password', function() {
+      var u = {
+        name: 'bla',
+        firstName: 'blabla',
+        email: 'a@a.com',
+        password: '123456',
+        teamId: 2
+      };
+      
+      before(function(done) {
+        User.forge(u).save().then(function() { done(); });
       });
-      it('should compare passwords', function() {
-        var u = {
-          name: 'bla',
-          firstName: 'blabla',
-          email: 'a@a.com',
-          password: '123456',
-          teamId: 2
-        };
 
-        return User.forge(u).save()
-        .then(function(saved) {
-          return User.findOne({id: saved.id}).then(function(ret) {
-            return ret.comparePassword(u.password).should.finally.equal(true);
-          });
+      after(function(done) {
+        return knex('users').truncate().then(function() { done(); });
+      });
+
+
+      it('should encrypt password', function() {
+        return User.findOne({email: u.email}).then(function(ret) {
+          return ret.get('password').should.not.be.equal('123456');
         });
+      });
 
+      it('should compare passwords', function() {
+        return User.findOne({email: u.email}).then(function(ret) {
+          return ret.comparePassword(u.password).should.finally.equal(true);
+        });
       });
     });
   });
