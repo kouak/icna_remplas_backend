7cb40e1daa3802874df08ad3f3f02c821d04681c
Benjamin Beret <benjamin.beret@gmail.com>
2015-05-07 22:05:07 +0200

    Allow CORS in development

--- server.js
+++ server.js
@@ -24,6 +24,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+if (app.get('env') === 'development') {
+  app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    next();
+  });
+}
+
 // Load static app
 app.use(express.static(path.join(__dirname, 'frontend')));
 
